d04d2436fd2558b07d31b836f3ed63d5b9e92b6f
wiliami <wiliaup97@gmail.com>
2023-10-26 00:02:59 -0400

    add exercises variables js

---
+++ exercicios.js
@@ -0,0 +1,10 @@
+var firstName = 'Wiliamis';
+var lastName = 'Oliveira';
+
+var address = 'Rua dos Dogs';
+
+address = 'Rua dos gatinhos';
+
+console.log(firstName); // Wiliamis
+console.log(lastName); // Oliveira
+console.log(address); // rua dos gatinhos
